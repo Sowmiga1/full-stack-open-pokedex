752af239a6c25fbd261e491f54d9584cec63a524
Sowmiga1 <sowmigaanand@gmail.com>
2024-10-20 19:18:43 +0530

    build webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: [".*", ".js", ".jsx"],
+    extensions: [".*", ".js", ".jsx",".mjs"],
   },
   devServer: {
     historyApiFallback: true,
